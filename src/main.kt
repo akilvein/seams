b70d666c69109bc33ee9cc22a7d8e8db1be75518
Kilvein Andrey <akilvein@saber3d.ru>
2020-02-11 15:35:48 +0300

    continue shortest path

--- src/main.kt
+++ src/main.kt
@@ -1,29 +1,117 @@
 import java.awt.Color
 import java.awt.Dimension
 import java.awt.Graphics
-import java.awt.image.BufferedImage
-import java.io.File
-import javax.imageio.ImageIO
+import java.util.*
 import javax.swing.JFrame
 import javax.swing.JPanel
 
-fun shortestPath(picture: BufferedImagePicture) : List<Int> {
-    fun weight(x: Int, y: Int) = when (y) {
-        0 -> 0.0
-        in 1 .. picture.height -> picture.pixelEnergy(x, y + 1)
-        picture.height + 1 -> 0.0
-        else -> IndexOutOfBoundsException("x must be between 0 and ${picture.width + 1}")
+
+fun getSeam(picture: BufferedImagePicture) : Array<Int> {
+    val distances = Array(picture.width) { DoubleArray(picture.height + 2) { Double.MAX_VALUE } }
+
+    class PixelNode(val x: Int, val y: Int) : Comparable<PixelNode> {
+        override fun compareTo(other: PixelNode): Int {
+            return distances[x][y].compareTo(distances[other.x][other.y])
+        }
     }
-    val result = mutableListOf<Int>()
 
+    val preceding = Array(picture.width) { Array<PixelNode?>(picture.height + 2) { null } }
+    val processed = Array(picture.width) { BooleanArray(picture.height + 2) { false } }
 
+    val source = PixelNode(0,0)
+    distances[0][0] = 0.0
+    val q = PriorityQueue<PixelNode>(picture.width * picture.height)
+    q.add(source)
 
+    fun weight(x: Int, y: Int): Double = when (y) {
+        0 -> 0.0
+        in (1 .. picture.height) -> picture.pixelEnergy(x, y - 1)
+        picture.height + 1 -> 0.0
+        else -> throw IndexOutOfBoundsException("y must be in [0 .. ${picture.height + 1}]")
+    }
 
-    return result
+    fun relaxPixel(x: Int, y: Int, current: PixelNode) {
+        val newDistance = distances[current.x][current.y] + weight(x, y)
+        if (newDistance < distances[x][y]) {
+            distances[x][y] = newDistance
+            preceding[x][y] = current
+            q.add(PixelNode(x, y))
+        }
+    }
+
+    fun unwindPath(): Array<Int> {
+        val result = Array(picture.height) { -1 }
+
+        var x = picture.width - 1
+        var y = picture.height + 1
+
+        while (y > 0) {
+            val p = preceding[x][y]!!
+            x = p.x
+            y = p.y
+
+            if (y - 1 in result.indices) {
+                result[y - 1] = x
+            }
+        }
+
+        return result
+    }
+
+    while (q.isNotEmpty()) {
+        val pixel = q.poll()
+
+        // we can have duplicates in queue
+        if (processed[pixel.x][pixel.y]) {
+            continue
+        }
+
+        if (pixel.x == picture.width - 1 && pixel.y == picture.height + 1) {
+            // woohoo!
+            break
+        }
+
+        processed[pixel.x][pixel.y] = true
+
+        //right for top/bottom rows
+        if ((pixel.y == -1 || pixel.y == picture.height) && pixel.x < picture.width - 1) {
+            relaxPixel(pixel.x + 1, pixel.y, pixel)
+        }
+
+        //down
+        if (pixel.y <= picture.height) {
+            relaxPixel(pixel.x, pixel.y + 1, pixel)
+
+            // down-left
+            if (pixel.x > 0) {
+                relaxPixel(pixel.x - 1, pixel.y + 1, pixel)
+            }
+
+            // down-right
+            if (pixel.x < picture.width - 1) {
+                relaxPixel(pixel.x + 1, pixel.y + 1, pixel)
+            }
+        }
+    }
+
+    return unwindPath()
+}
+
+fun pictureWithSeam(filename: String): BufferedImagePicture {
+    val image = BufferedImagePicture(filename)
+    val seam = getSeam(image)
+
+    for (y in seam.indices) {
+        val x = seam[y]
+        image[x, y] = Color.RED
+    }
+
+    return image
 }
 
 class ImagePanel : JPanel() {
-    private val image = BufferedImagePicture("dog.jpg").energyImage()
+    //private val image = BufferedImagePicture("dog.jpg").energyImage()
+    private val image = pictureWithSeam("surf.png")
 
     init {
         preferredSize = Dimension(image.width, image.height)
@@ -39,7 +127,7 @@ class ImagePanel : JPanel() {
      */
 }
 
-fun main(args: Array<String>) {
+fun main() {
     val frame = JFrame()
     val imagePanel = ImagePanel()
     frame.contentPane.add(imagePanel)
